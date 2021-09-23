6b46cfa9ac0c3221c61d5f9c580aa51a4dc9f59f
Juanra Dikal <j.hernandez@upc.edu>
2021-09-23 07:57:27 +0200

    updated deps

--- dist/bundles/esm.js
+++
@@ -1,1 +0,0 @@
-function o(o){const e=`Hello ${o}!`;return console.log(`Browser says "${e}"`),e}export{o as helloWorld};
--- dist/bundles/iife.js
+++
@@ -1,1 +0,0 @@
-var myPackageName=function(e){"use strict";return e.helloWorld=function(e){const o=`Hello ${e}!`;return console.log(`Browser says "${o}"`),o},Object.defineProperty(e,"__esModule",{value:!0}),e}({});
